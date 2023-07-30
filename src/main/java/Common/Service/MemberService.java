86393b8f618b87d3760b67c84c4a8f7a27ad6a7b
woo0121 <dntjd6070@naver.com>
2023-07-30 23:36:51 +0900

    123

--- src/main/java/Common/Service/MemberService.java
+++ src/main/java/Common/Service/MemberService.java
@@ -51,91 +51,11 @@ public class MemberService {
 	    MemberDao Dao = new MemberDao();
 	    int eDao = Dao.email(email);
 	    if (eDao == 0) {
-	    	//인증코드 생성
-			String AuthenticationKey = authCodeMaker();
-			
-			// mail server 설정
-		
-			String host = "smtp.gmail.com";
-			String user = "qmflcl072165@gmail.com"; // 자신의 구글 계정
-			String password = "mbvaoqnyrwldhvg";// 자신의 구글 패스워드
-			
-			// 메일 받을 주소
-			String to_email = inputedEmail;
-			System.out.println("inputedEmail : " + inputedEmail);
-
-			// SMTP 서버 정보를 설정한다.
-			Properties prop = System.getProperties();
-			prop.put("mail.smtp.host", host);
-			//google - TLS : 587, SSL: 465
-			prop.put("mail.smtp.port", 465);
-			prop.put("mail.smtp.starttls.enable", "true");
-			prop.put("mail.smtp.auth", "true");
-			prop.put("mail.smtp.ssl.enable", "true");
-			prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-			prop.put("mail.debug", "true");
-	        
-			Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(user, password);
-				}
-			});
-			MimeMessage msg = new MimeMessage(session);
-			
-			// email 전송
-			try {
-				msg.setFrom(new InternetAddress(user));
-				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to_email));
-
-				// 메일 제목
-				msg.setSubject("안녕하세요. 너공나공의 인증메일입니다.", "UTF-8");
-				// 메일 내용
-				msg.setText("인증 번호 :" + AuthenticationKey );
-
-				Transport.send(msg);
-				System.out.println("이메일 전송 : " + AuthenticationKey);
-				ShareVar_login sv = ShareVar_login.getInstance();
-				sv.authEmailCode = AuthenticationKey;
-
-			} catch (AddressException e) { 
-				// TODO Auto-generated catch block 
-				e.printStackTrace(); 
-			} catch (MessagingException e) { 
-					// TODO Auto-generated catch block 
-					e.printStackTrace(); 
-			}
-		}
-	} 
-	    public String authCodeMaker() {
-			String authCode = null;
-			
-			StringBuffer temp = new StringBuffer();
-			Random rnd = new Random();
-			for (int i = 0; i < 10; i++) {
-				int rIndex = rnd.nextInt(3);
-				switch (rIndex) {
-				case 0:
-					// a-z
-					temp.append((char) ((int) (rnd.nextInt(26)) + 97));
-					break;
-				case 1:
-					// A-Z
-					temp.append((char) ((int) (rnd.nextInt(26)) + 65));
-					break;
-				case 2:
-					// 0-9
-					temp.append((rnd.nextInt(10)));
-					break;
-				}
-			}
-			
-			authCode = temp.toString();
-			System.out.println(authCode);
-			
-			return authCode;
-		}
+//	    	23123
+	    }
+		return eDao;
+	}	
 }
-	
 	    	
 	    	
 	    
